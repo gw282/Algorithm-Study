abd319477ae67f0571536273a2713fa5cd0f1cce
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-12-03 17:09:43 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241203/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void PrintNLines(int n) {
+    for(int i=0;i<n;i++) {
+        cout << "12345^&*()_" <<'\n';
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    PrintNLines(n);
+    return 0;
+}
