79db1383853fa86815d53a9a55094c80db452635
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-12-04 23:35:46 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241204/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
+++ 241204/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 
 string PrintYesNo(int n) {
-    if(n % 5 == 0 && n % 2 == 0) return "Yes";
-    else "No";
+    int count = 0;
+    while(n) {
+        count += n%10;
+        n = n/10;
+    }
+    if(count % 5 == 0) return "Yes";
+    else return "No";
 }
 
 int main() {
     int n;
     cin >> n;
-
-    cout << PrintYesNo(n);
+    if(n%2 == 1) cout << "No";
+    else cout << PrintYesNo(n);
     return 0;
 }
