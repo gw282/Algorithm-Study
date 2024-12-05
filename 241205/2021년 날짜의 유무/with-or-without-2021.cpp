28e356da96257f7a648866421a413f07a72684fa
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-12-05 23:56:46 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241205/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 241205/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 string DateAvail(int a, int b) {
-    if(a > 13 || b > 31) return "No";
+    if(a > 12 || b > 31) return "No";
     if(a == 2 && b > 28) return "No";
     if(a == 4 && b > 30) return "No";
     if(a == 6 && b > 30) return "No";
