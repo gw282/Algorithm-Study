8c0c4197204ba14232813d30be7c7f9deedfde8a
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-12-11 19:29:12 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241211/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
+++ 241211/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -5,19 +5,15 @@ int n;
 
 void func(int n) {
     if(n==0) return;
+
     cout << n << ' ';
     func(n-1);
-}
-
-void func2(int m) {
-    if(m > n) return;
-    cout << m << ' ';
-    func2(m+1); 
+    cout << n << ' ';
 }
 
 int main() {
     cin >> n;
     func(n);
-    func2(1);
+
     return 0;
 }
