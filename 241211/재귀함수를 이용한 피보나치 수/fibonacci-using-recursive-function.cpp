8d4584c0fb3c2900982d6e807cb8472a7ed6748c
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-12-11 19:59:59 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241211/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int func(int n) {
+    if(n==1 || n==2) return 1;
+
+    return func(n-1) + func(n-2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << func(n);
+    return 0;
+}
