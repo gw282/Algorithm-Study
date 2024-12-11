f9a40f17bcc151e43a41d07bbe00f1dddc24499e
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-12-11 19:36:15 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241211/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int func(int n) {
+    if(n == 1) return 1;
+
+    return func(n-1) + n;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << func(n);
+    return 0;
+}
