5df618c78c96f6bc70d8fddc5c7fa2f7bd04e984
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-12-16 21:19:37 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 19ms, Memory: 1MB, Status: Passed - CodeTree

--- 241216/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
+++ 241216/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -16,6 +16,13 @@ int main() {
             cout << "No"; return 0;
         }
     }
+
+    for(int i=0;i<s2.length();i++) {
+        if(s1[i] == s2[i]) continue;
+        else {
+            cout << "No"; return 0;
+        }
+    }
     cout << "Yes";
     return 0;
 }
