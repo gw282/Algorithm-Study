719cdee92f74882c59d460b609f20c9968148523
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-12-18 19:58:42 +0900

    Title: 원점으로부터의 거리, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241218/원점으로부터의 거리/distance-from-origin.cpp
+++ 241218/원점으로부터의 거리/distance-from-origin.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>  // abs() 함수 사용을 위해 추가
 using namespace std;
 
 class Circle {
     public:
-        int x,y,num;
+        int x, y, num;
 
         Circle(int x, int y, int num) {
             this->x = x;
@@ -15,8 +16,16 @@ class Circle {
         Circle() {}
 };
 
-bool cmp(Circle a, Circle b) { 
-    return abs(a.x) + abs(a.y) < abs(b.x) + abs(b.y);
+// 비교 함수
+bool cmp(Circle a, Circle b) {
+    int distanceA = abs(a.x) + abs(a.y);
+    int distanceB = abs(b.x) + abs(b.y);
+    
+    if (distanceA == distanceB) {
+        return a.num < b.num;  // 거리 같으면 번호가 작은 순으로 정렬
+    }
+    
+    return distanceA < distanceB;  // 거리 기준으로 정렬
 }
 
 int main() {
@@ -27,11 +36,13 @@ int main() {
 
     for(int i = 0; i < n; i++) {
         cin >> circles[i].x >> circles[i].y;
-        circles[i].num = i+1;
+        circles[i].num = i + 1;  // 점 번호는 1부터 시작
     }
 
-    sort(circles.begin(), circles.end(), cmp); 
+    // 정렬
+    sort(circles.begin(), circles.end(), cmp);
 
+    // 출력
     for(int i = 0; i < n; i++) {
         cout << circles[i].num << endl;
     }
