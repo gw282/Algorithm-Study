6a630a8c1279fdaeea7be2ecaed7b5a2ba0c8bdb
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-12-19 21:28:31 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241219/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int month[13] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+    string day[7] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+    int m1,d1,m2,d2;
+    string day_selected;
+    cin >> m1 >> d1 >> m2 >> d2 >> day_selected;
+
+    int days1 = d1, days2 = d2, diff, sum = 0;
+
+    for(int i=1;i<m1;i++) {
+        days1 += month[i];
+    }
+
+    for(int i=1;i<m2;i++) {
+        days2 += month[i];
+    }
+
+    diff = days2 - days1;
+
+    sum += diff / 7;
+    diff = diff % 7;
+    
+    for(int i=0;i<diff;i++) {
+        if(day[i] == day_selected) sum +=1;
+    }
+    cout << sum;
+    return 0;
+}
