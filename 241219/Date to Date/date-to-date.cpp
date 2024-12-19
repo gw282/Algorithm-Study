9f63fb634ab4f4399f7a79f6b8047ce2e5bc6882
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-12-19 20:51:13 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241219/Date to Date/date-to-date.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m1,d1,m2,d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int day = 0;
+    if(m1 == m2) {
+        day = d2- d1;
+    } else {
+        if(m1 == 1 || m1 == 3 || m1 == 5 || m1 == 7 || m1 == 8 || m1 == 10 || m1 == 12) {
+            day += (32 - d1 + d2);
+        } else if(m1 == 2) {
+            day += (29 - d1 + d2);
+        } else {
+            day += (31 - d1 + d2);
+        }
+        m1 += 1;
+
+        while(m1 != m2) {
+            if(m1 == 1 || m1 == 3 || m1 == 5 || m1 == 7 || m1 == 8 || m1 == 10 || m1 == 12) {
+                day += 31;
+            } else if(m1 == 2) {
+                day += 28;
+            } else {
+                day += 30;
+            }
+            m1++;
+        }
+    }
+
+    cout << day;
+    return 0;
+}
