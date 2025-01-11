f483dcb2fecc67158892497dc1cc35723cfcc7bb
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2025-01-11 23:49:08 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 12ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250111/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#define MAX_R 2000
+#define OFFSET 1000
+
+using namespace std;
+
+int x1, y1, x2, y2;
+int x3, y3, x4, y4;
+
+int checked[MAX_R + 1][MAX_R + 1] ={0,};
+
+int main() {
+    
+    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> x3 >> y3 >> x4 >> y4;
+
+    x1 += OFFSET;
+    y1 += OFFSET;
+    x2 += OFFSET;
+    y2 += OFFSET;
+    x3 += OFFSET;
+    y3 += OFFSET;
+    x4 += OFFSET;
+    y4 += OFFSET;
+
+    for(int i=x1;i<x2;i++) {
+        for(int j=y1;j<y2;j++) {
+            checked[i][j] = 1;
+        }
+    }
+
+    for(int i=x3;i<x4;i++) {
+        for(int j=y3;j<y4;j++) {
+            checked[i][j] = 0;
+        }
+    }
+
+    int area = 0;
+    for(int i = 0; i <= MAX_R; i++)
+        for(int j = 0; j <= MAX_R; j++)
+            if(checked[i][j] == 1)
+                area++;
+    
+    cout << area;
+    
+    return 0;
+}
