30e4eeb10d6e87c4e87c45fd0a2cee177f67d0ae
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2025-01-11 22:50:44 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250111/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int x1[202], y1[202];
+int x2[202], y2[202];
+int board[202][202];
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
+
+        for (int x = x1[i] + 100; x < x2[i] + 100; x++) {
+            for (int y = y1[i] + 100; y < y2[i] + 100; y++) {
+                board[x][y]++;
+            }
+        }
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i <= 200; i++) {
+        for (int j = 0; j <= 200; j++) {
+            if (board[i][j] >= 1) ans++;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
