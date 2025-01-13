9c99ba17f9ae3d1874907d60cf58b1f3cdf647b6
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2025-01-13 20:57:42 +0900

    Title: 선두를 지켜라, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250113/선두를 지켜라/keep-the-lead.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int locA[1002];
+int locB[1002];
+
+int n, m;
+int v, t;
+bool overtake = true;
+int ans = -1;
+int main() {
+    cin >> n >> m;
+
+    int dis = 0;
+    for(int i=0;i<n;i++) {
+        cin >> v >> t;
+        while(t--) {
+            dis++;
+            locA[dis] = locA[dis-1] + v;
+        }
+    }
+
+    dis = 0;
+    for(int i=0;i<m;i++) {
+        cin >> v >> t;
+        while(t--) {
+            dis++;
+            locB[dis] = locB[dis-1] + v;
+        }
+    }
+
+    for(int i=1;i<=dis;i++) {
+        if(locA[i] == locB[i]) continue;
+        else if(locA[i] > locB[i] && ans == -1) {
+            ans++; overtake = true;
+        }
+        else if(locA[i] < locB[i] && ans == -1) {
+            ans++; overtake = false;
+        }
+        else if(locA[i] > locB[i] && overtake == false) {
+            ans++; overtake = true;
+        }
+        else if(locA[i] < locB[i] && overtake == true) {
+            ans++; overtake = false;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
